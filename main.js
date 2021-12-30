bfb849e9fab4b45a3b9af7c07b039012788426f0
Santasri97 <90270303+Santasri97@users.noreply.github.com>
2021-12-30 19:54:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ function update() {
   if (i > 5) {
     i = 0;
   }
-  var updatedImage = [images];
-  var updatedName = [names];
-  document.getElementById("family_member_image").src = updatedImage,[i];
-  document.getElementById("family_member_name").innerHTML= updatedName,[i];
+  var updatedImage = images[i];
+  var updatedName = names[i];
+  document.getElementById("family_member_image").src = updatedImage;
+  document.getElementById("family_member_name").innerHTML= updatedName;
   }
   
   
